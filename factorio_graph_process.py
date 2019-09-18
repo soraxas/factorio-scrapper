f4bf83bc0744bc8e4e37b955c188aa9640feb809
Oscar Lai <tlai4178@uni.sydney.edu.au>
2019-09-18 16:38:14 +1000

    working scrapper and post-processing of the dataset into graphviz grpah

---
+++ factorio_graph_process.py
@@ -0,0 +1,402 @@
+import pickle
+
+from factorio_scrapper import Item, Row, ItemFromFactorioIcon, Recipe
+
+# with open('factorio.pkl', 'rb') as f:
+#     visited_link, items = pickle.load(f)
+with open('fact.pkl', 'rb') as f:
+    visited_link, items = pickle.load(f)
+
+
+
+
+#%%
+
+all_recipes = set([item['Recipe'] for k, item in items.items() if 'Recipe' in item.values])
+all_req_tech = [(item['Required technologies'], item) for k, item in items.items() if 'Required technologies' in item.values]
+all_prod_by = [(item['Produced by'], item) for k, item in items.items() if 'Produced by' in item.values]
+
+
+LI = ItemFromFactorioIcon.from_values
+
+# manually add recipe
+
+extra_recipes = [
+    Recipe.from_values([LI('Heavy oil', 20), LI('Time', 20)],
+                       [LI('Solid fuel', 1)]),
+    Recipe.from_values([LI('Light oil', 10), LI('Time', 20)],
+                       [LI('Solid fuel', 1)]),
+    Recipe.from_values([LI('Petroleum gas', 20), LI('Time', 20)],
+                       [LI('Solid fuel', 1)]),
+    Recipe.from_values([LI('Crude oil', 100), LI('Water', 50), LI('Time', 5)],
+                       [LI('Petroleum gas', 55), LI('Heavy oil',25), LI('Light oil', 45)]),
+]
+
+all_recipes.update(extra_recipes)
+
+
+#%%
+
+from graphviz import Digraph
+
+dot = Digraph(comment='factorio')
+
+_nodes = set()
+_edges = set()
+
+FILTER_OUT = {
+    'Time',
+    'barrel',
+    'Empty barrel'
+}
+
+def add_edge(e1, e2, *args, **kwargs):
+    if e1 in FILTER_OUT or e2 in FILTER_OUT:
+        return False
+    dot.edge(e1, e2, *args, **kwargs)
+    _edges.add((e1, e2))
+    return True
+
+# dir(dot)
+# dot.edge(set(c.name for c in r.conditions), tuple(e.name for c in r.effects))
+#
+#
+# tuple(c.name for c in r.conditions)
+# tuple(e.name for c in r.effects)
+
+
+phantom_nodes = []
+def get_phantom_nodes():
+    p = f'_phantom_node{len(phantom_nodes)}'
+    phantom_nodes.append(p)
+    return p
+
+#%%
+
+
+for r in all_recipes:
+    p1 = get_phantom_nodes()
+    p2 = get_phantom_nodes()
+
+    # if len(r.conditions) == len(r.effects) == 1:
+    #     #  one to one mapping
+    #     add_edge(c.conditions[0].name, r.effects[0], color='blue')
+    # else:
+    for c in r.conditions:
+        add_edge(c.name, p1, str(c.count), dir='none', color='blue')
+    if len(r.effects) == 1:
+        add_edge(p1, r.effects[0].name, str(r.effects[0].count), color='red')
+    else:
+        for e in r.effects:
+            add_edge(p2, e.name, str(e.count), color='red')
+        add_edge(p1, p2, dir='none', color='red')
+
+used_edges = set()
+for e in _edges:
+    used_edges.update(e)
+for p in phantom_nodes:
+    if p in used_edges:
+        dot.node(p, shape='point', width='0.01', height='0.01')
+
+#%%
+"""ORIGINAL ADD RECIPE"""
+# for r in all_recipes:
+#     for c in r.conditions:
+#         for e in r.effects:
+            # add_edge(c.name, e.name, color='blue')
+
+"""REQ TECH"""
+if False:
+    for req_tech, item in all_req_tech:
+        for r in req_tech:
+            add_edge(r.name, item.name, color='green')
+
+# we need to do some filtering first
+_edges_to_be_process = {}
+for prod_by, item in all_prod_by:
+    for p in prod_by:
+        if item.name in FILTER_OUT or p.name in FILTER_OUT:
+            continue
+        _p_out_edge = _edges_to_be_process.get(item.name, [])
+        _p_out_edge.append(p.name)
+        _edges_to_be_process[item.name] = _p_out_edge
+        # _edges_to_be_process.append((p.name, item.name))
+        # add_edge(p.name, item.name, color='purple')
+# try to add manual crafting first, if not exist then assembling machine 1..3
+for dest, incomings in _edges_to_be_process.items():
+    incomings = list(filter(lambda x : 'barrel' not in x, incomings))
+    if 'Crafting#Manual crafting' in incomings:
+        # we treat manual crafting as do-able always
+        continue
+    if len(incomings) == 1:
+        true_incoming = incomings[0]
+    elif 'Assembling machine 1' in incomings:
+        true_incoming = 'Assembling machine 1'
+    elif 'Assembling machine 2' in incomings:
+        true_incoming = 'Assembling machine 2'
+    elif 'Assembling machine 3' in incomings:
+        true_incoming = 'Assembling machine 3'
+    elif 'Oil refinery' in incomings:
+        true_incoming = 'Oil refinery'
+    elif 'Stone furnace' in incomings:
+        true_incoming = 'Oil refinery'
+    else:
+        raise Exception((dest, incomings))
+
+    add_edge(true_incoming, dest, color='purple')
+    dot.node(true_incoming, style='filled', fillcolor='maroon1')
+
+#%%
+for e in _edges:
+    _nodes.update(e)
+
+def check_not_exists_in_all_edges(name, idx):
+    """Given an index, this will check if there exists no given name
+    among the idex of all edges."""
+    for e in _edges:
+        if n == e[idx]:
+            return False
+    return True
+
+for n in _nodes:
+    # color node without incomings (root)
+    if check_not_exists_in_all_edges(name=n, idx=1):
+        dot.node(n, style='filled', fillcolor='green3')
+    # color node without outcomings (leaves)
+    if check_not_exists_in_all_edges(name=n, idx=0):
+        dot.node(n, style='filled', fillcolor='aquamarine')
+
+#%%
+
+# dot.save('factorio.dot')
+dot.render('factorio', view=True)
+
+#%%
+
+
+
+def use_igraph():
+    import igraph
+
+    vertices = list(items.keys())
+    sorted(vertices)
+
+    G = igraph.Graph(directed=True)
+    # G.add_vertices(vertices)
+
+    vertices_used = []
+
+    edges = []
+
+    for item in items.values():
+        # try:
+        if 'Recipe' in item.values:
+            edge_a = item.item_url_name
+            for _i in item.values['Recipe']:
+                edge_b = _i.item_url_name
+
+                # print(edge_a, edge_b)
+
+
+                # assert edge_a in vertices, edge_a
+                # assert edge_b in vertices, edge_b
+                if edge_a not in vertices_used:
+                    vertices_used.append(edge_a)
+                if edge_b not in vertices_used:
+                    vertices_used.append(edge_b)
+
+
+                edges.append((edge_b, edge_a))
+                # edges.append((edge_a, edge_b))
+                # edges.append((vertices_used.index(edge_a), vertices_used.index(edge_b)))
+        # except KeyError:
+        #     continue
+        #     pass
+        # a
+        # a
+        # G.add_edges()
+    G.add_vertices(vertices_used)
+    G.add_edges(edges)
+
+    G.vs[0]
+
+
+    G.vs['label'] = vertices
+    G.vs['shape'] = ['rectangle'] * len(vertices)
+    G.vs['label_size'] = [10] * len(vertices)
+    G.vs['size'] = [30] * len(vertices)
+    G.vs['size2'] = [2] * len(vertices)
+
+    igraph.plot(
+                G,
+                margin=150,
+                bbox=(1500, 1500),  #layout=G.layout("kk"),
+                # bbox=(300, 300),  #layout=G.layout("kk"),
+                # mark_groups=
+                # group,
+                # collections.OrderedDict(reversed(list(group.items()))),
+                # shapes=[2] * 100,  # square
+                # **plot_ig_kwargs,
+                autocurve=True)
+
+
+
+def previous_trial():
+
+    def add_node(color=None):
+        if edge_a not in _nodes:
+            _nodes.add(edge_a)
+            dot.node(edge_a, color=color)
+        if edge_b not in _nodes:
+            _nodes.add(edge_b)
+            dot.node(edge_b, color=color)
+
+
+    BLACK_LIST = [
+        'Advanced_oil_processing',
+        'Barrel',
+        'Coal_liquefaction',
+        'Crafting#Manual_crafting',
+        'Empty_barrel',
+        'Empty_water_barrel',
+        'Fill_crude_oil_barrel',
+        'Fill_heavy_oil_barrel',
+        'Fill_light_oil_barrel',
+        'Fill_lubricant_barrel',
+        'Fill_petroleum_gas_barrel',
+        'Fill_sulfuric_acid_barrel',
+        'Fill_water_barrel',
+        'Heavy_oil_cracking',
+        'Light_oil_cracking',
+        'Oil_processing',
+        'Oil_processing#Recipes',
+        'Solid_fuel_from_heavy_oil',
+        'Solid_fuel_from_light_oil',
+        'Time',
+        'Solid_fuel_from_petroleum_gas',
+     ]
+
+
+    i = 0
+
+    {i['Prototype type'] for i in items.values() if 'Prototype type' in i.values}
+
+    for k, item in items.items():
+        i += 1
+        # if i > 80:
+        #     break
+        # try:
+
+
+        try:
+            if k in BLACK_LIST:
+                continue
+
+            # if item['Prototype type'] not in (
+            #     'resource',
+            #     'recipe',
+            #     'item',
+            #     'tool',
+            # ):
+            #     continue
+
+            edge_a = item['Internal name']
+            # edge_a = item.item_url_name
+
+            if 'Recipe' in item.values:
+                item['Recipe'].condition
+
+            for row_type in [
+                # 'Recipe',
+                             # 'Required technologies',
+                'Produced by',
+                             # 'Consumed by',
+                             ]:
+
+
+
+                if row_type in item.values:
+                    for _i in item[row_type]:
+                        # print(_i.item_url_name)
+
+                        if _i.item_url_name == 'Crafting#Manual_crafting':
+                            print(item)
+
+                        if _i.item_url_name in BLACK_LIST:
+                            continue
+
+
+                        edge_b = items[_i.item_url_name]['Internal name']
+                        # edge_b = _i.item_url_name
+
+
+                        if row_type == 'Recipe':
+                            color='blue'
+                        elif row_type == 'Required technologies':
+                            color='red'
+                        elif row_type == 'Produced by':
+                            color='green'
+                        elif row_type == 'Consumed by':
+                            color='brown'
+                        else:
+                            raise Exception()
+
+                        edge_added = False
+
+
+
+                        if row_type in (
+                            'Recipe',
+                            'Required technologies',
+                            'Produced by'
+                            ):
+                            e = edge_b, edge_a
+                            dot.edge(edge_b, edge_a, color=color)
+                            edge_added=True
+                        elif row_type in (
+                            '___',
+                            'Consumed by',
+                            # ,
+                            ):
+                            e = edge_a, edge_b
+                            dot.edge(edge_a, edge_b, color=color)
+                            edge_added=True
+
+                        _edges.add(e)
+
+
+                        if edge_added:
+                            _nodes.add(edge_a)
+                            _nodes.add(edge_b)
+
+                        if row_type == 'Produced by':
+                            break
+
+        except KeyError as e:
+            raise e
+            print(str(e))
+        except AssertionError as ee:
+            # raise e
+            # print(e)
+            print(row_type)
+
+    i
+
+    len(_nodes)
+
+    for n in _nodes:
+        # check if it has incoming edge
+        color = 'red'
+        for e in _edges:
+            if e[1] == n:
+                color = 'white'
+
+        if n in [
+            'copper-ore',
+            'iron-ore',
+            'stone',
+            'coal',
+        ]:
+            color = 'red'
+
+        dot.node(n, style='filled', fillcolor=color)
---
+++ factorio_scrapper.py
@@ -0,0 +1,339 @@
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import selenium.common.exceptions as seleniumException
+
+kwargs = {
+    'executable_path': 'bin/phantomjs',
+    # 'desired_capabilities': dcap,
+    # 'service_log_path': log_path
+}
+
+# webdriver.Chrome(**kwargs)
+
+driver = webdriver.PhantomJS(**kwargs)
+
+#%%
+
+
+# dir(driver)
+
+rows_to_retrieve = [
+    'Recipe',
+    'Total raw',
+    'Stack size',
+    'Prototype type',
+    'Internal name',
+    'Required technologies',
+    'Produced by',
+    'Required technologies',
+    'Fuel value',
+    'Used as fuel by',
+    # 'Mining time',
+    # 'Vehicle acceleration',
+    'Consumed by',
+    ]
+
+multi_item_types = ['Recipe',
+                    'Total raw',
+                    'Required technologies',
+                    'Produced by',
+                    'Consumed by',]
+
+for i in multi_item_types:
+    assert i in rows_to_retrieve
+
+# %%%
+
+class Row():
+    def __init__(self, web_element):
+        # self.web_element = web_element
+        self.items = [ItemFromFactorioIcon(e) for e in
+                      web_element.find_elements_by_class_name('factorio-icon')]
+
+    def __repr__(self):
+        return f'<Row: {self.items}>'
+
+    def __iter__(self):
+        return self.items.__iter__()
+
+    def __getitem__(self, idx):
+        return self.items[idx]
+
+    @staticmethod
+    def from_values(items):
+        new_item = Row.__new__(Row)
+        new_item.items = items
+        return new_item
+
+class ItemFromFactorioIcon():
+    def __init__(self, web_element):
+        # self.web_element = web_element
+        self.name = web_element.find_element_by_tag_name('a').get_attribute('title')
+        self.link = web_element.find_element_by_tag_name('a').get_attribute('href')
+        self.count = web_element.find_element_by_class_name('factorio-icon-text').text
+
+    @property
+    def item_url_name(self):
+        return self.link.split('/')[-1]
+
+    def __repr__(self):
+        count = self.count
+        if count == '':
+            return f"<LinkedItem:{self.name}>"
+        else:
+            return f"<LinkedItem:{self.name}={self.count}>"
+
+    def __eq__(self, other):
+        return self.name == other.name and self.count == other.count
+
+    @staticmethod
+    def from_values(name, count, link=None):
+        new_item = ItemFromFactorioIcon.__new__(ItemFromFactorioIcon)
+        new_item.name = name
+        new_item.link = link
+        new_item.count = count
+        return new_item
+
+    def __hash__(self):
+        return hash((self.name, self.link, self.count))
+
+
+class Item():
+    """Represent an item, and store all of the important information."""
+    def __init__(self, web_element):
+        # self.web_element = web_element
+        self.name = web_element.title.split(' - ')[0]
+        self.item_url_name = web_element.current_url.split('/')[-1]
+        self.values = {}
+        for row in rows_to_retrieve:
+            try:
+                self.values[row] = get_value(row)
+            except AssertionError:
+                pass
+
+    def __getitem__(self, idx):
+        return self.values[idx]
+
+    def __repr__(self):
+        text = '\n'.join([f'      {k}: {v}' for k,v in self.values.items()])
+        return f'<item:{self.name}\n{text}\n\item>'
+
+    def __hash__(self):
+        return hash(self.item_url_name)
+
+    @staticmethod
+    def from_values(item_url_name, values):
+        new_item = Item.__new__(Item)
+        new_item.item_url_name = item_url_name
+        new_item.values = values
+        return new_item
+
+def get_row_from_title(title, idx_to_return=None, return_next_row=False):
+    """This helper function helps to locate a row title,
+    and then return the following row (if the optional argument is given,
+    because they are separated as two different row)
+    if idx_to_return is set, return if have multiple items
+    ."""
+
+    xpath = f"//tr[contains(@class, 'border-top') and starts-with(normalize-space(.), '{title}')]"
+    element = driver.find_elements_by_xpath(xpath)
+    if idx_to_return is None:
+        assert len(element) == 1, f"{len(element)}, Not haing exactly one matching row"
+        idx_to_return = 0
+    else:
+        assert len(element) > 0
+    if return_next_row:
+        # return immediate next row
+        return driver.find_elements_by_xpath(f'{xpath}/following-sibling::tr')[idx_to_return]
+    else:
+        return element[idx_to_return]
+
+def get_value(row_type):
+    """Return the correct value depend on row type."""
+    if row_type == 'Recipe':
+        return Recipe(get_row_from_title(row_type, return_next_row=True, idx_to_return=0))
+    elif row_type in multi_item_types:
+        return Row(get_row_from_title(row_type, return_next_row=True, idx_to_return=0))
+    else:
+        values = get_row_from_title(row_type).text.split('\n')[1:]
+        assert len(values) == 1, values
+        return values[0]
+
+
+class Recipe(Row):
+
+    def __init__(self, web_element):
+        super().__init__(web_element)
+        self.items = [ItemFromFactorioIcon(e) for e in
+                      web_element.find_elements_by_class_name('factorio-icon')]
+        # extract conditions and effects
+        self.operators = self.get_equation_effects(web_element.text)
+        assert len(self.items) == len(self.operators)
+        self.conditions = [self.items[i] for i in range(len(self.items))
+                           if self.operators[i] == 'condition']
+        self.effects = [self.items[i] for i in range(len(self.items))
+                           if self.operators[i] == 'effect']
+
+    @staticmethod
+    def get_equation_effects(equation):
+        current_operator = 'condition'
+        operators = []
+        eq_split = equation.split('\n')
+        # make sure we can make each odd element as float (as they must be qualtity)
+        for i in range(len(eq_split)):
+            if i % 2 == 0: # we are testing even instead of odd because of 0-base indexing
+                operators.append(current_operator)
+            else:
+                if eq_split[i] not in ('+', '→'):
+                    raise Exception(f'{repr(eq_split)} at {i} as {repr(eq_split[i])}')
+                if eq_split[i] == '→':
+                    current_operator = 'effect'
+        return operators
+
+    def __hash__(self):
+        return hash((frozenset(self.conditions), frozenset(self.effects)))
+
+    def __eq__(self, other):
+        if len(self.conditions) != len(other.conditions):
+            return False
+        if len(self.effects) != len(other.effects):
+            return False
+        for i in range(len(self.conditions)):
+            if self.conditions[i] != other.conditions[i]:
+                return False
+        for i in range(len(self.effects)):
+            if self.effects[i] != other.effects[i]:
+                return False
+        return True
+
+    def __repr__(self):
+        linked_item_tostr = lambda x : f'<{x.name}={x.count}>'
+        return (f"<Recipe: {' + '.join(map(linked_item_tostr, self.conditions))}"
+               f" → {' + '.join(map(linked_item_tostr, self.effects))}>")
+
+    def __iter__(self):
+        return self.items.__iter__()
+
+    def __getitem__(self, idx):
+        return self.items[idx]
+
+    @staticmethod
+    def from_values(conditions, effects):
+        new_item = Recipe.__new__(Recipe)
+        new_item.items = []
+        new_item.items.extend(conditions)
+        new_item.items.extend(effects)
+        new_item.conditions = conditions
+        new_item.effects = effects
+        return new_item
+
+# %%
+# get_row_from_title('Recipe', return_next_row=True, idx_to_return=0).text
+def scrap():
+    items = {}
+    links_to_visit = ['https://wiki.factorio.com/Wood']
+    # links_to_visit = ['https://wiki.factorio.com/Uranium-238']
+    # links_to_visit = ['https://wiki.factorio.com/Combat_shotgun']
+    visited_link = set()
+
+    while len(links_to_visit):
+
+        link = links_to_visit.pop(0)
+        if link in visited_link:
+            continue
+        visited_link.add(link)
+
+        print(f'[{len(visited_link)}] visiting {link}...')
+        driver.get(link)
+
+        item = Item(driver)
+        items[item.item_url_name] = item
+        for field in multi_item_types:
+            if field in item.values:
+                links_to_visit.extend((linked_item.link for linked_item in item.values[field]))
+
+# import pickle
+#
+#
+# with open('fact.pkl', 'wb') as f:
+#     # pickle.dump((visited_link, from_json(data)), f)
+#     pickle.dump((visited_link, items), f)
+
+
+# %%
+#
+
+def to_json(x):
+    """Convert the nested scrapped items dict to json text"""
+    y = {}
+    if isinstance(x, Recipe)  or str(type(x)) == "<class '__main__.Recipe'>":
+        y['type'] =  'Recipe'
+        y['items'] = list(map(to_json, x.items))
+        y['operators'] = x.operators
+        y['conditions'] = list(map(to_json, x.conditions))
+        y['effects'] = list(map(to_json, x.effects))
+
+    elif isinstance(x, Row) or str(type(x)) == "<class '__main__.Row'>":
+        y['type'] =  'Row'
+        y['items'] = list(map(to_json, x.items))
+
+    elif isinstance(x, Item) or str(type(x)) == "<class '__main__.Item'>":
+        y['type'] =  'Item'
+        y['item_url_name'] =  x.item_url_name
+        y['values'] = to_json(x.values)
+
+    elif isinstance(x, ItemFromFactorioIcon) or str(type(x)) == "<class '__main__.ItemFromFactorioIcon'>":
+        y['type'] =  'ItemFromFactorioIcon'
+        y['name'] =  x.name
+        y['link'] =  x.link
+        y['count'] =  x.count
+
+    elif isinstance(x, dict):
+        y =  {k : to_json(v) for k,v in  x.items()}
+
+    elif isinstance(x, (str, float, int)):
+        y = x
+    else:
+        raise Exception(type(x))
+
+    return y
+
+def from_json(x):
+    """Convert the nested scrapped items dict to """
+    if isinstance(x, dict):
+        if 'type' not in x:
+            return {k : from_json(v) for k, v in x.items()}
+
+        elif x['type'] == 'Recipe':
+            return Recipe.from_values(
+                conditions=[from_json(_x) for _x in x['conditions']],
+                effects=[from_json(_x) for _x in x['effects']])
+
+        elif x['type'] == 'Row':
+            return Row.from_values([from_json(_x) for _x in x['items']])
+
+        elif x['type'] == 'Item':
+            return Item.from_values(
+                item_url_name=x['item_url_name'],
+                values=from_json(x['values']))
+
+        elif x['type'] == 'ItemFromFactorioIcon':
+            return ItemFromFactorioIcon.from_values(
+                name=x['name'],
+                count=x['count'],
+                link=x['link'])
+
+    elif isinstance(x, (str,)):
+        return x
+
+    else:
+        print(x)
+        raise Exception(type(x))
+
+# import json
+#
+# with open("factorioaaa.json", "w") as write_file:
+#     json.dump(to_json(items), write_file)
+#
+# with open("factorio.json", "r") as write_file:
+#     data = json.load(write_file)
